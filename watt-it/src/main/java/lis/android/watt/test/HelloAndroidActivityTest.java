3703a75cad6394dd71b2e3af594682add20d8a4c
Łukasz Lis <lis.lukasz85@gmail.com>
2012-12-17 18:18:14 +0100

    Initial commit
    Basic project setup to start work.

---
+++ watt-it/src/main/java/lis/android/watt/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package lis.android.watt.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import lis.android.watt.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
